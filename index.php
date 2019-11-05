4a371cb3839c6c85438e417127d59d9bf47855bb
Aresh Saharkhiz <saharkiz@yahoo.com>
2019-11-05 21:17:13 +0400

    Add files via upload

--- index.php
+++ index.php
@@ -189,7 +189,7 @@
 		            <div class="block-18">
 		            	<div class="icon"><span class="flaticon-doctor"></span></div>
 		              <div class="text">
-		                <strong class="number" data-number="705">0</strong>
+		                <strong class="number" data-number="68">0</strong>
 		                <span>Projects Completed</span>
 		              </div>
 		            </div>
@@ -198,7 +198,7 @@
 		            <div class="block-18">
 		            	<div class="icon"><span class="flaticon-doctor"></span></div>
 		              <div class="text">
-		                <strong class="number" data-number="809">0</strong>
+		                <strong class="number" data-number="12">0</strong>
 		                <span>Satisfied Customer</span>
 		              </div>
 		            </div>
@@ -207,8 +207,8 @@
 		            <div class="block-18">
 		            	<div class="icon"><span class="flaticon-doctor"></span></div>
 		              <div class="text">
-		                <strong class="number" data-number="335">0</strong>
-		                <span>Awwards Received</span>
+		                <strong class="number" data-number="34">0</strong>
+		                <span>Awards Received</span>
 		              </div>
 		            </div>
 		          </div>
@@ -216,7 +216,7 @@
 		            <div class="block-18">
 		            	<div class="icon"><span class="flaticon-doctor"></span></div>
 		              <div class="text">
-		                <strong class="number" data-number="35">0</strong>
+		                <strong class="number" data-number="12">0</strong>
 		                <span>Years of Experienced</span>
 		              </div>
 		            </div>
@@ -313,7 +313,7 @@
 				<div class="row no-gutters justify-content-center mb-5">
           <div class="col-md-7 text-center heading-section ftco-animate">
             <h2 class="mb-4">Our Recent Projects</h2>
-            <p>Separated they live in. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country</p>
+            <p></p>
             <p></p>
           </div>
         </div>
@@ -408,7 +408,7 @@
 				<div class="row justify-content-center mb-5 pb-2">
           <div class="col-md-8 text-center heading-section ftco-animate">
             <h2 class="mb-4"><span>Recent</span> Blog</h2>
-            <p>Separated they live in. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country</p>
+            <p></p>
           </div>
         </div>
 				<div class="row">
@@ -424,13 +424,6 @@
               <div class="text bg-white p-4">
                 <h3 class="heading"><a href="#">Finance And Legal Working Streams Occur Throughout</a></h3>
                 <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-                <div class="d-flex align-items-center mt-4">
-	                <p class="mb-0"><a href="#" class="btn btn-primary">Read More <span class="ion-ios-arrow-round-forward"></span></a></p>
-	                <p class="ml-auto mb-0">
-	                	<a href="#" class="mr-2">Admin</a>
-	                	<a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a>
-	                </p>
-                </div>
               </div>
             </div>
           </div>
@@ -446,13 +439,6 @@
               <div class="text bg-white p-4">
                 <h3 class="heading"><a href="#">Finance And Legal Working Streams Occur Throughout</a></h3>
                 <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-                <div class="d-flex align-items-center mt-4">
-	                <p class="mb-0"><a href="#" class="btn btn-primary">Read More <span class="ion-ios-arrow-round-forward"></span></a></p>
-	                <p class="ml-auto mb-0">
-	                	<a href="#" class="mr-2">Admin</a>
-	                	<a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a>
-	                </p>
-                </div>
               </div>
             </div>
           </div>
@@ -468,13 +454,6 @@
               <div class="text bg-white p-4">
                 <h3 class="heading"><a href="#">Finance And Legal Working Streams Occur Throughout</a></h3>
                 <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-                <div class="d-flex align-items-center mt-4">
-	                <p class="mb-0"><a href="#" class="btn btn-primary">Read More <span class="ion-ios-arrow-round-forward"></span></a></p>
-	                <p class="ml-auto mb-0">
-	                	<a href="#" class="mr-2">Admin</a>
-	                	<a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a>
-	                </p>
-                </div>
               </div>
             </div>
           </div>
@@ -486,8 +465,8 @@
       <div class="container">
         <div class="row justify-content-center mb-5">
           <div class="col-md-8 text-center heading-section ftco-animate">
-            <h2 class="mb-4">Our Clients Says</h2>
-            <p>Separated they live in. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country</p>
+            <h2 class="mb-4">Our Clients Say</h2>
+            <p>We discuss your business module and provide a plan that best evolves your business online and creates maximum ROI</p>
           </div>
         </div>
         <div class="row ftco-animate justify-content-center">
@@ -501,9 +480,9 @@
                   	<span class="quote d-flex align-items-center justify-content-center">
                       <i class="icon-quote-left"></i>
                     </span>
-                    <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-                    <p class="name">Racky Henderson</p>
-                    <span class="position">Father</span>
+                    <p>We love our new booking system</p>
+                    <p class="name">Scubadiving.ae</p>
+                    <span class="position">Founder</span>
                   </div>
                 </div>
               </div>
@@ -515,9 +494,9 @@
                   	<span class="quote d-flex align-items-center justify-content-center">
                       <i class="icon-quote-left"></i>
                     </span>
-                    <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-                    <p class="name">Henry Dee</p>
-                    <span class="position">Businesswoman</span>
+                    <p>Amazing Work on our website. our sales have increased online</p>
+                    <p class="name">Littlethingsme.com</p>
+                    <span class="position">Business</span>
                   </div>
                 </div>
               </div>
@@ -529,36 +508,8 @@
                   	<span class="quote d-flex align-items-center justify-content-center">
                       <i class="icon-quote-left"></i>
                     </span>
-                    <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-                    <p class="name">Mark Huff</p>
-                    <span class="position">Businesswoman</span>
-                  </div>
-                </div>
-              </div>
-              <div class="item">
-                <div class="testimony-wrap d-flex">
-                  <div class="user-img" style="background-image: url(images/person_4.jpg)">
-                  </div>
-                  <div class="text pl-4">
-                  	<span class="quote d-flex align-items-center justify-content-center">
-                      <i class="icon-quote-left"></i>
-                    </span>
-                    <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-                    <p class="name">Rodel Golez</p>
-                    <span class="position">Businesswoman</span>
-                  </div>
-                </div>
-              </div>
-              <div class="item">
-                <div class="testimony-wrap d-flex">
-                  <div class="user-img" style="background-image: url(images/person_1.jpg)">
-                  </div>
-                  <div class="text pl-4">
-                  	<span class="quote d-flex align-items-center justify-content-center">
-                      <i class="icon-quote-left"></i>
-                    </span>
-                    <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-                    <p class="name">Ken Bosh</p>
+                    <p>Our website has increased our booking and online presence</p>
+                    <p class="name">Bluewingdubai.com</p>
                     <span class="position">Businesswoman</span>
                   </div>
                 </div>
@@ -578,38 +529,22 @@
             	<h2 class="ftco-heading-2">Have a Questions?</h2>
             	<div class="block-23 mb-3">
 	              <ul>
-	                <li><span class="icon icon-map-marker"></span><span class="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
-	                <li><a href="#"><span class="icon icon-phone"></span><span class="text">+2 392 3929 210</span></a></li>
-	                <li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@yourdomain.com</span></a></li>
+	                <li><span class="icon icon-map-marker"></span><span class="text">Dubai UAE</span></li>
+	                <li><a href="#"><span class="icon icon-phone"></span><span class="text">+97156 272 8117</span></a></li>
+	                <li><a href="#"><span class="icon icon-envelope"></span><span class="text">saharkiz@gmail.com</span></a></li>
 	              </ul>
 	            </div>
             </div>
           </div>
           <div class="col-md-6 col-lg-3">
             <div class="ftco-footer-widget mb-5">
-              <h2 class="ftco-heading-2">Recent Blog</h2>
-              <div class="block-21 mb-4 d-flex">
-                <a class="blog-img mr-4" style="background-image: url(images/image_1.jpg);"></a>
-                <div class="text">
-                  <h3 class="heading"><a href="#">Even the all-powerful Pointing has no control about</a></h3>
-                  <div class="meta">
-                    <div><a href="#"><span class="icon-calendar"></span> June 27, 2019</a></div>
-                    <div><a href="#"><span class="icon-person"></span> Admin</a></div>
-                    <div><a href="#"><span class="icon-chat"></span> 19</a></div>
-                  </div>
-                </div>
-              </div>
-              <div class="block-21 mb-5 d-flex">
-                <a class="blog-img mr-4" style="background-image: url(images/image_2.jpg);"></a>
-                <div class="text">
-                  <h3 class="heading"><a href="#">Even the all-powerful Pointing has no control about</a></h3>
-                  <div class="meta">
-                    <div><a href="#"><span class="icon-calendar"></span> June 27, 2019</a></div>
-                    <div><a href="#"><span class="icon-person"></span> Admin</a></div>
-                    <div><a href="#"><span class="icon-chat"></span> 19</a></div>
-                  </div>
-                </div>
-              </div>
+              <h2 class="ftco-heading-2">Services</h2>
+              <ul class="list-unstyled">
+                <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Dynamic Website</a></li>
+                <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Mobile Application</a></li>
+                <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>eCommerce Website</a></li>
+                <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Company Portal</a></li>
+              </ul>
             </div>
           </div>
           <div class="col-md-6 col-lg-3">
@@ -625,15 +560,6 @@
             </div>
           </div>
           <div class="col-md-6 col-lg-3">
-            <div class="ftco-footer-widget mb-5">
-            	<h2 class="ftco-heading-2">Subscribe Us!</h2>
-              <form action="#" class="subscribe-form">
-                <div class="form-group">
-                  <input type="text" class="form-control mb-2 text-center" placeholder="Enter email address">
-                  <input type="submit" value="Subscribe" class="form-control submit px-3">
-                </div>
-              </form>
-            </div>
             <div class="ftco-footer-widget mb-5">
             	<h2 class="ftco-heading-2 mb-0">Connect With Us</h2>
             	<ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-3">
